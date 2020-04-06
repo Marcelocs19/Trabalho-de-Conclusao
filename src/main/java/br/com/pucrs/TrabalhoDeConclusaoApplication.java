7b6a299fcfbb43ee9fc1347b28c67d7d26acc203
Marcelo <m_campos_19@hotmail.com>
2020-04-06 19:24:47 -0300

    :tada: Inicio do projeto

---
+++ src/main/java/br/com/pucrs/TrabalhoDeConclusaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pucrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDeConclusaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDeConclusaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pucrs/TrabalhoDeConclusaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pucrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDeConclusaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
