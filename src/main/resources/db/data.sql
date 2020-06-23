a6b2c17fa0d2307eb98a9d6d2f662eecdd419749
Marcelo <m_campos_19@hotmail.com>
2020-06-22 21:51:16 -0300

    ✨ alterando dados do banco

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO PROFESSOR(id,nome,sobrenome,email,senha) VALUES(1, 'Marcelo', 'Silva', 'marcelo@gmail.com', '123456') ON CONFLICT DO NOTHING;
 
-INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(1, 'Fulano', 'Da Silva', 'Rua Guilherme Alves', 230, '58798549065', 'teste1@gmail.com') ON CONFLICT DO NOTHING;
-INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(2, 'Beltrano', 'Oliveria', 'Av. Ipiranga', 7200, '75269096056', 'teste2@gmail.com') ON CONFLICT DO NOTHING;
-INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(3, 'Ciclano', 'Silva', 'Rua Machado de Assis', 313, '87521626052', 'teste3@gmail.com') ON CONFLICT DO NOTHING;
+INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(1, 'João', 'Da Silva', 'Rua Guilherme Alves', 230, '58798549065', 'teste1@gmail.com') ON CONFLICT DO NOTHING;
+INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(2, 'Guilherme', 'Oliveria', 'Av. Ipiranga', 7200, '75269096056', 'teste2@gmail.com') ON CONFLICT DO NOTHING;
+INSERT INTO ALUNO(id,nome,sobrenome,endereco,numero,cpf,email) VALUES(3, 'Eliane', 'Rocha', 'Rua Machado de Assis', 313, '87521626052', 'teste3@gmail.com') ON CONFLICT DO NOTHING;
