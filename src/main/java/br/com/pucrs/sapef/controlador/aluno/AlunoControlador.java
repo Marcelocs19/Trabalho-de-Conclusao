1cc188b6625fa19beb842c520ab310604a59ddd9
Marcelo <m_campos_19@hotmail.com>
2020-06-29 13:40:22 -0300

    ✨ Adicionado método que exclui um aluno e adicionado status do travis

--- src/main/java/br/com/pucrs/sapef/controlador/aluno/AlunoControlador.java
+++ src/main/java/br/com/pucrs/sapef/controlador/aluno/AlunoControlador.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ public class AlunoControlador {
 	private static final String LISTAR_ALUNOS = "/listarAlunos";
 	private static final String CADASTRAR_ALUNO = "/novo";
 	private static final String EDITAR_ALUNO = "/editar/{id}";
+	private static final String EXCLUIR_ALUNO = "/excluir/{id}";
 	
 	@Autowired
 	private AlunoServico alunoServico;
@@ -44,6 +46,16 @@ public class AlunoControlador {
 	@PutMapping(EDITAR_ALUNO) 
 	public ResponseEntity<AlunoDto> editarAluno(@PathVariable(name = "id") Long id, @RequestBody @Valid AlunoForm aluno) {
 		return ResponseEntity.ok().body(alunoServico.editarAluno(id, aluno));
+	}
+	
+	@DeleteMapping(EXCLUIR_ALUNO)
+	public ResponseEntity<Boolean> excluirAluno(@PathVariable(name = "id") Long id) {
+		Boolean excluirAluno = alunoServico.excluirAluno(id);
+		if(Boolean.TRUE.equals(excluirAluno)) {
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
